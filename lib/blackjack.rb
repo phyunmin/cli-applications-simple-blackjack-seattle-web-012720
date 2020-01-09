5a49b85bc268d5d0636318836322b383fb86eeab
Hyunmin Park <hyunmin.park.career@gmail.com>
2020-01-09 20:52:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -69,7 +69,7 @@ def runner
   card_total = initial_round
   
   until card_total>21 do
-    card_total = hit?(init)
+    card_total = hit?(card_total)
     display_card_total(card_total)
     end_game(card_total)
   end
